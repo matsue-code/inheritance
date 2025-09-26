7ea16b08a97d8725634ddf2da8cae74741670629
matsue-code <mbolamathew@icloud.com>
2025-09-26 20:39:09 +0300

    second commit

---
+++ main2.py
@@ -0,0 +1,14 @@
+class person(object):
+    def __init__(self,name,idnumber):
+        self.name=name
+        self.idnumber=idnumber
+    def display(self):
+        print(self.name)
+        print(self.idnumber)
+class employee(person):
+    def __init__(self,name,idnumber,salary,post):
+        self.salary=salary
+        self.post=post
+        person.__init__(self,name,idnumber)
+a = employee('rahul',46464,64644,"intern")
+a.display()
